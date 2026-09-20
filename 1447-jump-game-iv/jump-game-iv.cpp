e8639b147e2ef4be9b532fb4c5118821023117b9
Purushotam Pathak <purushotampathak85@gmail.com>
2026-09-20 23:46:59 +0530

    Time: 124 ms (73.01%) | Memory: 111.7 MB (75.16%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.cpp
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -1,61 +1,59 @@
 class Solution {
 public:
-    int solve_BFS(vector<int>& arr, int& n) {
-        vector<bool> visited(n, false);
-        unordered_map<int, vector<int>> mp; 
-        
-        for(int i = 0; i<n; i++) {
-            mp[arr[i]].push_back(i);
-        }
-        
-        queue<int> que;
-        que.push(0);
-        visited[0] = true;
-        int steps = 0;
-        
-        while(!que.empty()) {
-            int size = que.size();
-        
-            while(size--) {
-                int curr = que.front();
-                que.pop();
-                
-                if(curr == n-1) {
-                    return steps;
-                }
-                
-                int left  = curr-1;
-                int right = curr+1;
-                
-                if(left >= 0 && !visited[left]) {
-                    que.push(left);
-                    visited[left] = true;
-                }
-                
-                if(right < n && !visited[right]) {
-                    que.push(right);
-                    visited[right] = true;
-                }
-                
-                for(const int &idx : mp[arr[curr]]) {
-                    if(!visited[idx]) {
-                        que.push(idx);
-                        visited[idx] = true;
-                    }
-                }
-                
-                mp.erase(arr[curr]);
-            }
-            steps++;
-        }
-        
-        return -1;
-    }
-
     int minJumps(vector<int>& arr) {
         int n = arr.size();
-        if(n == 1)
+
+        if (n == 1)
             return 0;
-        return solve_BFS(arr, n);
+
+        unordered_map<int, vector<int>> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[arr[i]].push_back(i);
+        }
+
+        vector<bool> visited(n, false);
+        queue<int> q;
+
+        q.push(0);
+        visited[0] = true;
+
+        int steps = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+
+            while (size--) {
+                int i = q.front();
+                q.pop();
+
+                if (i == n - 1)
+                    return steps;
+
+                if (i - 1 >= 0 && !visited[i - 1]) {
+                    visited[i - 1] = true;
+                    q.push(i - 1);
+                }
+
+                if (i + 1 < n && !visited[i + 1]) {
+                    visited[i + 1] = true;
+                    q.push(i + 1);
+                }
+
+                if (mp.count(arr[i])) {
+                    for (int j : mp[arr[i]]) {
+                        if (!visited[j]) {
+                            visited[j] = true;
+                            q.push(j);
+                        }
+                    }
+                    mp.erase(arr[i]);
+                }
+            }
+
+            steps++;
+        }
+
+        return -1;
     }
 };
